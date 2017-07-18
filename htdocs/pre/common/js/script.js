b75e924fc285a795ff656ce9460a6e0058f9b181
kurahara <kurahara@unitea.jp>
2017-07-18 15:48:36 +0900

    AJ-2 HTMLコーディング

--- htdocs/pre/common/js/script.js
+++ htdocs/pre/common/js/script.js
@@ -23,3 +23,17 @@ $(function(){
         $('#header').toggleClass('is-active');
     });
 });
+function changeTwitterWidgetDesign(){
+  var $twitter_widget = $('iframe.twitter-timeline');
+  var $twitter_widget_contents = $twitter_widget.contents();
+  
+  if ($twitter_widget.length > 0 && $twitter_widget[0].contentWindow.document.body.innerHTML !== ""){
+    $twitter_widget_contents.find('head').append('<link href="common/css/twitter.css" rel="stylesheet" type="text/css">');
+  }
+  else {
+    setTimeout(function(){
+      changeTwitterWidgetDesign();
+    }, 350);
+  }
+}
+changeTwitterWidgetDesign();
